5fd0f9e311a6ba6c29844efdb06ad3d5dc99f9f6
Ananya Jain <ananya1473.be20@chitkara.edu.in>
2022-10-17 01:04:15 +0530

    Routing and added image to Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import AddUser from "./components/AddUser"
+import About from "./components/About"
+import Home from "./components/Home"
+import Navbar from "./components/Navbar"
+import {Route , Routes} from "react-router-dom"
 
 function App() {
   return (
     
     <div className="App">
-    <AddUser/>
+    <Navbar/>
+    <div className="container">
+      <Routes>
+        <Route path = "/about" element = {<About/>}/>
+        <Route path = "/registration" element = {<AddUser/>}/>
+        <Route path = "/" element = {<Home/>}/>
+
+      </Routes>
+      
+       </div>
     </div>
   )
 }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+    <div className='img'>
+    <img src="/images/librarymanagementssystem.png" alt = "image"/>
+    </div>
+    </>
+  )
+}
+
+export default Home
